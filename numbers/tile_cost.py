3cfffbc9c6d1f8bd3390d1b22dda8572a8f32d48
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-04-10 14:45:02 -0400

    Solved: Tile Cost Calculator
    This was not too bad at all. Surprisingly spent more time on the actual
    error handling than anything else. Mostly just trying to get an output I
    liked. Seems to work well enough but I'm already noticing room for
    improvement.

---
+++ numbers/tile_cost.py
@@ -0,0 +1,34 @@
+import sys
+
+
+if __name__ == '__main__':
+    # Very simple argument parsing. I wanted to do it all my own here rather
+    # than require something like `click`. I can definitely improve on this.
+    # Quick but functional grab of command line arguments. If one doesn't
+    # exist, fail entirely.
+    try:
+        width, length, unit_price = sys.argv[1:]
+    except ValueError as e:
+        print(f'\n{sys.argv[0]} WIDTH LENGTH UNIT_PRICE\n')
+        print('Something went wrong!')
+        print(e)
+        # Exit status 2: command line usage error. Arguments invalid.
+        exit(2)
+
+    try:
+        square_feet = int(width) * int(length)
+        total_cost = int(unit_price) * square_feet
+    except ValueError as e:
+        print('Something went wrong!')
+        print(e)
+        print('Please check your arguments, and try again.')
+        # Exit status 1: failure, as defined by the (this!) program.
+        exit(1)
+
+    print('Cost Estimate')
+    print('-------------')
+    print(f'  For room sized {width}ft by {length}ft,')
+    print(f'  with a total of {square_feet} sq. ft.')
+    print(f'  and a price of ${unit_price} per sq. ft.')
+    print(f'  The price of tile should be around ${total_cost},')
+    print(f'  not including any taxes or other fees.')
